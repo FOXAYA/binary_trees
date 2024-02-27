7ab6d787d87b2b06f0b16aafa32b27c3e5949661
FOXAYA <omniasaged111@gmail.com>
2024-02-27 04:30:13 -0800

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -19,7 +19,7 @@ typedof struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}binary_tree_t;
+}
 
 void binary_tree_print(const binary_tree_t *);
 
