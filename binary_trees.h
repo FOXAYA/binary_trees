71cc519aaf63e98b843629cd1591c451143fc8a0
FOXAYA <omniasaged111@gmail.com>
2024-02-27 07:31:28 -0800

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -13,19 +13,21 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedof struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}
-
-void binary_tree_print(const binary_tree_t *);
+};
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+/**************************mandorty tasks d*******************************/
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif /* BINARY_TREES_H */
 
