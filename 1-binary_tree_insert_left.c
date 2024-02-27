40e21909a94b213a74ae882c886034beea84ee49
FOXAYA <omniasaged111@gmail.com>
2024-02-27 08:07:25 -0800

    updated

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,16 +10,16 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	new_node = binary_tree_node(parent, value);
-	if(new_node == NULL)
+	if (new_node == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	if (parent->left != NULL)
 	{
